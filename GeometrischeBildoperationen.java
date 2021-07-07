0191c46fb7d9b6addff6a1fa90e93a34d16b3e8e
kaursumandeep <suman.pabla2001@gmail.com>
2021-07-07 17:18:34 +0200

    07.07

--- GeometrischeBildoperationen.java
+++ GeometrischeBildoperationen.java
@@ -130,17 +130,44 @@ public class GeometrischeBildoperationen  implements Bildoperation
         return neuesBild;
     }
 
-      public picture faltung(Picture originalbild, double [][] filter){
+      public Picture faltung(Picture originalbild, double [][] filter){
           int lange= filter.length;
           int halb=lange/2;
           
-          color [][] pixel=originalBild.getPixelsTableColor();
-          color [][] pixelNeu= new Color [breite][hoehe];
-          for (int x=halb ; x< originalBild.getWidth()-halb;x++){
-              for (int y= halb ; y> originalBild.getHeight()-halb;y++){
+          Color [][] pixel = originalBild.getPixelsTableColor();
+          Color [][] pixelNeu = new Color[breite][hoehe];
+          
+          for (int x = halb ; x < originalbild.getWidth()-halb;x++){
+              for (int y = halb ; y > originalbild.getHeight()-halb;y++){
+                  int xx = x - halb;
+                  int yy = y - halb;
+                  double rot = 0.0;
+                  double gruen =0.0;
+                  double blau = 0.0;
+                  for (int i = 0; i < laenge; i++){
+                    for (int j = 0; j <laenge; j++){
+                        rot += filter[i][j] * pixel[xx+i][yy+j].getRed();
+                        gruen += filter[i][j] * pixel[xx+i][yy+j].getGreen();
+                        blau += filter[i][j] * pixel[xx+i][yy+j].getBlue();
+                        
+                
           }
       }
     
-    
-    
- }
+      if (rot < 0.0) rot = 0.0;
+      if (rot > 1.0) rot = 1.0;
+      if (gruen < 0.0) gruen = 0.0;
+      if (gruen > 1.0) gruen = 1.0;
+      if (blau < 0.0) blau = 0.0;
+      if (blau > 1.0) blau = 1.0;
+      
+      pixelNeu[x][y]= new Color ( rot, gruen, blau, 1.0);
+    }
+}
+      Picture neuesBild = originalBild.copy();
+      neuesBild.setPixels(pixelNeu);
+      return neuesBild;
+    }
+}
+
+ 
